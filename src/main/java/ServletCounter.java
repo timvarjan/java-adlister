f9e29f6eb4e355b6a0714a1e72d286b72f253ac6
Timothy Varjan <tim.varjan@gmail.com>
2023-06-13 11:21:07 -0300

    servelets exercise

---
+++ src/main/java/ServletCounter.java
@@ -0,0 +1,30 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "CounterServlet", urlPatterns = "/count")
+public class ServletCounter extends HttpServlet {
+
+    private int count = 0;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+
+        count++;
+
+        String reset = req.getParameter("reset");
+
+        if (reset != null) {
+            count = 0;
+        }
+
+        out.println("<h1>" + count +"</h1>");
+
+    }
+}
